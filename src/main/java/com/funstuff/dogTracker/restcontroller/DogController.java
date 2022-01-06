481a2dcd005ed2df583dbb87d1e768c4fef00f8b
rbirnshtok <rbirnshtok@agilissystems.com>
2022-01-05 18:14:00 -0600

    Added dogTracker in the api path

--- src/main/java/com/funstuff/dogTracker/restcontroller/DogController.java
+++ src/main/java/com/funstuff/dogTracker/restcontroller/DogController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("dogTracker")
 public class DogController {
     @Autowired
     DogService dogService;
